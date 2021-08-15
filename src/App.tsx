36c783b3844d66a1012cb5c18ed6246935f2d219
Roy Alcala Ortiz <alcala.rao@gmail.com>
2021-08-14 19:20:00 -0500

    with typescript2

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with react, typescript and amplify
         </a>
       </header>
     </div>
